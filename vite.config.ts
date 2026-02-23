64cc3a2094e94bd8e2dabcbacfd4e84c1f7acbe4
Nedavniy <nedavniidanylo@gmail.com>
2026-02-23 04:02:58 +0100

    Initial commit — SmartTask fullstack demo

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';    
+import tailwindcss from '@tailwindcss/vite';         
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    tailwindcss(),   
     react()
   ],
-})
+});
